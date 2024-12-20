d5b3d793385b82f6d73dab8c08d2c5ecdf38c170
Your Name <bhargavnadoda.lw@gmail.com>
2024-12-20 15:56:36 +0530

    add package code and test code

--- lib/smoothimageview.dart
+++ lib/smoothimageview.dart
@@ -1,5 +1,80 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+// ignore_for_file: must_be_immutable
+
+// Dart imports:
+import 'dart:io';
+
+// Package imports:
+import 'package:cached_network_image/cached_network_image.dart';
+// Flutter imports:
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+
+enum ImageType { files, assets, networks }
+
+class SmoothImageView extends StatelessWidget {
+  String image;
+  ImageType type;
+  Widget? placeHolder;
+  double? height;
+  double? width;
+  BoxFit? fit;
+  Color? shimmerBaseColor;
+  Color? shimmerhighlightColor;
+
+  SmoothImageView({
+    super.key,
+    required this.image,
+    required this.type,
+    this.placeHolder,
+    this.height,
+    this.width,
+    this.fit = BoxFit.cover,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return switch (type) {
+      ImageType.assets => Image.asset(
+          image,
+          height: height,
+          width: width,
+          fit: fit,
+        ),
+      ImageType.files => Image.file(
+          File(image),
+          height: height,
+          width: width,
+          fit: fit,
+        ),
+      ImageType.networks => image.isEmpty
+          ? placeHolder ??
+              Shimmer.fromColors(
+                  baseColor: shimmerBaseColor ?? Colors.grey.shade300,
+                  highlightColor: shimmerhighlightColor ?? Colors.grey.shade900,
+                  child: SizedBox(
+                    height: height ?? 180,
+                    width: width ?? MediaQuery.of(context).size.width,
+                  ))
+          : CachedNetworkImage(
+              imageUrl: image,
+              height: height,
+              width: width,
+              fit: fit,
+              placeholder: (context, url) {
+                return placeHolder ??
+                    Shimmer.fromColors(
+                        baseColor: shimmerBaseColor ?? Colors.grey.shade300,
+                        highlightColor: shimmerhighlightColor ?? Colors.grey.shade900,
+                        child: SizedBox(
+                          height: height,
+                          width: width,
+                        ));
+              },
+              errorWidget: (context, url, error) {
+                return placeHolder ?? const Icon(Icons.error);
+              },
+            ),
+    };
+  }
 }
--- test/smoothimageview_test.dart
+++ test/smoothimageview_test.dart
@@ -1,12 +1,110 @@
+// smoothimageview_test.dart
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:smoothimageview/smoothimageview.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  // Tests for different scenarios of SmoothImageView
+
+  group('SmoothImageView Widget Tests', () {
+    testWidgets('renders asset image correctly', (WidgetTester tester) async {
+      // Arrange: Provide an asset image path
+      const image = 'assets/image.png';
+
+      // Act: Build the widget tree with an asset image type
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: SmoothImageView(
+              image: image,
+              type: ImageType.assets,
+            ),
+          ),
+        ),
+      );
+
+      // Assert: Check if the Image.asset widget is rendered
+      expect(find.byType(Image), findsOneWidget);
+      expect(find.byType(Image), findsOneWidget);
+      expect(
+          find.byWidgetPredicate(
+              (widget) => widget is Image && widget.image is AssetImage),
+          findsOneWidget);
+    });
+
+    testWidgets('renders file image correctly', (WidgetTester tester) async {
+      // Arrange: Provide a local file image path
+      const image = '/path/to/file/image.jpg';
+
+      // Act: Build the widget tree with a file image type
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: SmoothImageView(
+              image: image,
+              type: ImageType.files,
+            ),
+          ),
+        ),
+      );
+
+      // Assert: Check if Image.file widget is rendered
+      expect(find.byType(Image), findsOneWidget);
+      expect(
+          find.byWidgetPredicate(
+              (widget) => widget is Image && widget.image is FileImage),
+          findsOneWidget);
+    });
+
+    testWidgets('renders network image correctly with shimmer placeholder',
+        (WidgetTester tester) async {
+      // Arrange: Provide a network image URL
+      const image =
+          'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif';
+
+      // Act: Build the widget tree with a network image type and check for shimmer effect
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: SmoothImageView(
+              image: image,
+              type: ImageType.networks,
+            ),
+          ),
+        ),
+      );
+
+      // Assert: Check for the shimmer effect while loading
+      expect(find.byType(Shimmer), findsOneWidget);
+      expect(find.byType(CachedNetworkImage), findsOneWidget);
+    });
+
+
+    testWidgets('renders the provided placeholder widget while loading',
+        (WidgetTester tester) async {
+      // Arrange: Define a placeholder widget
+      const placeholder = CircularProgressIndicator();
+
+      // Act: Build the widget tree with a network image and custom placeholder
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: SmoothImageView(
+              image:
+                  'https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',
+              type: ImageType.networks,
+              placeHolder: placeholder,
+            ),
+          ),
+        ),
+      );
+
+      // Assert: Check if the provided placeholder widget is displayed while loading
+      expect(find.byWidget(placeholder), findsOneWidget);
+    });
   });
 }
